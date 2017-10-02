1592b84777bcce6bd9d25e51dc64ddf9c08eae65
Kevin Overtoom <kevin@inwave.nl>
2017-10-02 20:34:35 +0200

    twilio update

--- src/Auth/Gateways/Twilio.php
+++ src/Auth/Gateways/Twilio.php
@@ -1,21 +1,24 @@
-<?php 
+<?php
 
 namespace PaschalDev\Laravauth\Auth\Gateways;
 
-use Aloha\Twilio\Support\Laravel\Facade as TwilioMessenger;
 use PaschalDev\Laravauth\Contracts\TwoFactorSmsGateway;
+use Twilio\Rest\Client;
 
-class Twilio implements TwoFactorSmsGateway{
+class Twilio implements TwoFactorSmsGateway
+{
 
-	/**
+    /**
      * Uses Nexmo API to send texts to user's phone.
      *
-     * @param string $to 
+     * @param string $to
      * @param string $message
      * @return mixed
      */
-	public function send($to, $message){
-
-		return TwilioMessenger::message( $to, $message );
-	}
+    public function send($to, $message)
+    {
+        $sms = new Client(env('TWILIO_SID'), env('TWILIO_TOKEN'));
+        return $sms->messages->create($to,
+            ['from' => env('TWILIO_NUMBER'), 'body' => $message]);
+    }
 }
